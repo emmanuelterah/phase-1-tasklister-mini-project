3d824f48ca7ec831430155db9423d5b9914c421d
emmanuelterah <emmanuelterah98@gmail.com>
2024-01-04 13:05:43 +0300

    Completed the assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const taskInput = document.getElementById("new-task-description");
+    const newTask = taskInput.value;
+
+    const taskItem = document.createElement("li");
+    taskItem.textContent = newTask;
+
+    taskList.appendChild(taskItem);
+
+    taskInput.value = "";
+  });
 });
